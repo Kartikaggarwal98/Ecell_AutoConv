80702dc68b52462fb3e2b0d84837de05bd007e9b
kartik <kartikaggarwal98@gmail.com>
2016-10-09 11:03:04 +0530

    n

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from django.views import generic
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+
+def index(request):
+	context_dict={}
+	context_dict['hi']='hi'
+	return render(request,'dashboard/index.html',context_dict)
