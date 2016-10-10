0c3bd40321f89588b103ea72dcfad7e328a52127
kartik <kartikaggarwal98@gmail.com>
2016-10-10 22:12:04 +0530

    form submit

--- dashboard/views.py
+++ dashboard/views.py
@@ -6,24 +6,26 @@ from django.shortcuts import render,redirect
 from django.conf import settings
 
 from dashboard.models import Messages,User
-
+fb_id='hello'
 def index(request):
 	context_dict = {}
 	context_dict['hi'] = 'hi'
 	context_dict['data'] = Messages.objects.all()[::-1]
 	return render(request, 'dashboard/index.html', context_dict)
-fb_id=''
 def login(request):
 	context_dict = {}
 	if(request.method == 'GET'):
-		fb_id=request.GET.get('fb_id')
-		print "fb id ***",fb_id
-	# context_dict['fb_id']=fb_id
+		fb_id=str(request.GET.get('fb_id'))
+		print "fb id ***",fb_id,type(fb_id)
+		context_dict['fb_id']=fb_id
+
 	if(request.method == 'POST'):
 		email=request.POST.get('email')
 		batch=request.POST.get('batch')
+		fb_id=request.POST.get('fb_id')
 		context_dict['email']=email
 		context_dict['batch']=batch
+		context_dict['fb_id']=fb_id
 		p=User.objects.get_or_create(fb_id=fb_id,
 			email=email,
 			batch=batch)[0]
