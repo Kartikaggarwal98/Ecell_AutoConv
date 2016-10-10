95d490010433b1f0de3fd71521d3c45c43caadce
kartik <kartikaggarwal98@gmail.com>
2016-10-10 20:23:02 +0530

    form

--- codingblocks/urls.py
+++ codingblocks/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 from chatbot.views import MyChatBotView, index
 from dashboard.views import index as dashboard_index
-from dashboard.views import login
+from dashboard.views import log_in,auth_login
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'codingblocks.views.home', name='home'),
@@ -13,5 +13,5 @@ urlpatterns = patterns('',
     url(r'^facebook_auth/?$', MyChatBotView.as_view()),
     url(r'^$',index),
     url(r'^dashboard/?$',dashboard_index),
-    url(r'^login/?$',login)
+    url(r'^login/?$',log_in)
 )
