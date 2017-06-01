e3201327a1a72120be92cfbf2c27abc57a60c19d
kartik macpro <kartik@Apples-MacBook-Pro.local>
2017-06-02 00:35:38 +0530

    api ai

---
+++ apiaitest.py
@@ -0,0 +1,22 @@
+import apiai
+import os
+import sys
+import json
+
+
+
+
+# demo agent acess token: e5dc21cab6df451c866bf5efacb40178
+
+CLIENT_ACCESS_TOKEN = '0e76c8cfd8c44710aab2224c72d3aac7'
+ai = apiai.ApiAI(CLIENT_ACCESS_TOKEN)
+user_message = 'hi'
+
+        
+request = ai.text_request()
+request.query = user_message
+
+response = json.loads(request.getresponse().read())
+
+result = response['result']
+print result
