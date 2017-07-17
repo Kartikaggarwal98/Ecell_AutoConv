c306ef22617ea623fcfc3b21c6ec24c7067de7b3
kartik macpro <kartik@Apples-MacBook-Pro.local>
2017-07-17 14:04:37 +0530

    old spread try

--- chatbot/views.py
+++ chatbot/views.py
@@ -70,8 +70,7 @@ def save_message(fbid='1129928563722136',message_text='hi'):
     return json.dumps(data)
 
 def scrape_spreadsheet():
-    sheet_id='1c-8Zx-g0USneS5X9bThXf0G-O_NvXV6gT_k3VYnSIjE'
-    #sheet_id = '1EXwvmdQV4WaMXtL4Ucn3kwwhS1GOMFu0Nh9ByVCfrxk'
+    sheet_id = '1EXwvmdQV4WaMXtL4Ucn3kwwhS1GOMFu0Nh9ByVCfrxk'
     url = 'https://spreadsheets.google.com/feeds/list/%s/od6/public/values?alt=json'%(sheet_id)
 
     resp = requests.get(url=url)
@@ -164,20 +163,21 @@ def set_menu():
     logg(status.text,'-MENU-OBJECT-')
 
 
-def gen_response_object(fbid,item_type='members'):
+def gen_response_object(fbid,item_type='course'):
     spreadsheet_object = scrape_spreadsheet()
-    item_arr = [i for i in spreadsheet_object if i['etype'] == item_type]
+    item_arr = [i for i in spreadsheet_object if i['itemtype'] == item_type]
     elements_arr = []
 
     for i in item_arr:
         sub_item = {
-                        "title":i['ename'],
-                        "item_url":i['elink'],
-                        "image_url":i['epicture'],
+                        "title":i['itemname'],
+                        "item_url":i['itemlink'],
+                        "image_url":i['itempicture'],
+                        "subtitle":i['itemdescription'],
                         "buttons":[
                           {
                             "type":"web_url",
-                            "url":i['elink'],
+                            "url":i['itemlink'],
                             "title":"Open"
                           },
                           {
@@ -202,7 +202,7 @@ def gen_response_object(fbid,item_type='members'):
                 }
               }
             }
-    print "-------------response object-----------------"
+
     return json.dumps(response_object)
 
 def gen_answer_object(fbid,keyword='index error'):
@@ -300,8 +300,8 @@ def gen_answer_object(fbid,keyword='index error'):
 def post_facebook_message(fbid,message_text):
     post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
     message_text = message_text.lower()
-    #save_message(fbid,message_text)
-    if message_text in 'members,about,events'.split(','):
+    save_message(fbid,message_text)
+    if message_text in 'teacher,why,course'.split(','):
         response_msg = gen_response_object(fbid,item_type=message_text)
     elif message_text.startswith('/ask'):
         query = message_text.replace('/ask','')
@@ -321,9 +321,10 @@ def post_facebook_message(fbid,message_text):
       print(u"< %s" % output_text)     
       response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":output_text}})
 
+
     # else:
     #     output_text = "Hi, how may I help you"
-    print response_msg    
+    
     requests.post(post_message_url, 
                     headers={"Content-Type": "application/json"},
                     data=response_msg)
@@ -487,4 +488,3 @@ class MyChatBotView(generic.View):
                     logg(e,symbol='-332-')
 
         return HttpResponse()  
-
